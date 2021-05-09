a4e3ceb22295a9fbf843bc89b804cf3fd41b3aed
Simran-panwar <81704718+Simran-panwar@users.noreply.github.com>
2021-05-09 15:48:25 +0530

    Create Extension.py

---
+++ Extension.py
@@ -0,0 +1,9 @@
+fileName=input()
+Dict={"py":"python","c":"C","cc":"C++","jsp":"Java","html":"HTML","pl":"Perl","php":"PHP"}
+if '.' in fileName:
+    i=fileName.index('.')
+    extension=fileName[i+1:]
+    print("The extension of the file is: ")
+    print(Dict[extension])
+else:
+        print("INVALID INPUT")
